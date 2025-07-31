eea69ed824f7c1a8b271abb43b3ad353eb2364f9
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-07-31 14:12:31 -0400

    Create RegistrationForm.jsx

---
+++ src/components/RegistrationForm.jsx
@@ -0,0 +1,11 @@
+function RegisterForm() {
+  return (
+    <form>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default RegisterForm;
