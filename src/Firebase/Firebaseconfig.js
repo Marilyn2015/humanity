839fd7486aa4b559df89371f061513843047f9e8
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-07-31 17:03:15 -0400

    Update Firebaseconfig.js

--- src/Firebase/Firebaseconfig.js
+++ src/Firebase/Firebaseconfig.js
@@ -1,22 +1,25 @@
+// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBT7P7DAfV-I9ESe6f9Jdp5ioCyGIK0d9A",
   authDomain: "hashhumanity-58b9a.firebaseapp.com",
   databaseURL: "https://hashhumanity-58b9a-default-rtdb.firebaseio.com",
   projectId: "hashhumanity-58b9a",
-  storageBucket: "hashhumanity-58b9a.firebasestorage.app",
+  storageBucket: "hashhumanity-58b9a.appspot.com",
   messagingSenderId: "886745899016",
   appId: "1:886745899016:web:2b0f7e043c2434379d71bd",
   measurementId: "G-3ZTCXL4374"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getDatabase(app);
-const storage = getStorage(app);
-
-export { app, analytics, db, storage };
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
