f6e3167bb190607333dc56fcb5747b7920d665e0
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-07-31 11:47:05 -0400

    Create app.jsx

---
+++ scr/app.jsx
@@ -0,0 +1,12 @@
+import RegisterForm from "./components/RegisterForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Registration Portal</h1>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
