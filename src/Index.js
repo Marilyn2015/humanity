eb89e23cc9cd13a77586fd82a8211cc60be59036
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-01 09:46:23 -0400

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LandingPage from "./LandingPage";
+import LandingPage from "./components/LandingPage"; // 👈 updated path
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<LandingPage />);
