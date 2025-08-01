6488944413760e65915d8412da38ba153c249d2a
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-01 17:58:12 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+// src/index.js
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/humanity">
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
