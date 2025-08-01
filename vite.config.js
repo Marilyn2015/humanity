5043fd9d2579aa46ce00bd6f13f9e74fbc542ae2
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-01 17:52:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/humanity/',
   plugins: [react()],
-  base: '/humanity/', // <<< REQUIRED for GitHub Pages project sites
-})
->>>>>>> 41dcf794d50b30e1bb2c30f37dd693db8b254048
+});
+
