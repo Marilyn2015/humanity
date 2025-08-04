f53e1ec8ed1bf4a59fe9a8fc2605b686396568c8
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-04 09:04:26 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/humanity/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Only bundle the root index.html
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  }
+  base: '/Marilyn2015.github.io/humanity/',
 })
