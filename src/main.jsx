0b711f6e39cef07442f3962dcd3ce9f2cb5a9a15
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-08 15:17:45 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./App.jsx"; // App.jsx should be in src/
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
