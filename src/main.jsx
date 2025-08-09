85a0fb8d220885595eb34f11b72abf58d8ff7f4f
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-09 09:06:50 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import LandingPage from "./components/LandingPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx"; // create if you haven’t yet
+import UniversePage from "./pages/UniversePage.jsx";
+
+function AppRouter() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/universe" element={<UniversePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </HashRouter>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>
 );
