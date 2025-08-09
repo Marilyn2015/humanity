dc535b2f813ae07ee367ae8dc296756a6846b945
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-09 08:37:26 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+// src/App.jsx
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom"; 
-// HashRouter is safer for GitHub Pages
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import LandingPage from "./components/LandingPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+import LandingPage from "./components/LandingPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        {/* Catch-all: send unknown routes to landing */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
