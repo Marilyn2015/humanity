5e258ec04b3257726efd5261d7b7481122d4f8ef
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-09 09:01:59 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-// src/App.jsx
 import React from "react";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import LandingPage from "./components/LandingPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
+import UniversePage from "./pages/UniversePage.jsx"; // <-- this file, exact casing
 
 export default function App() {
   return (
@@ -13,6 +13,7 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/universe" element={<UniversePage />} /> {/* <-- added */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
