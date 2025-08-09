d778c927bbad4541a97cb575ba29b29f364ca8e1
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-09 08:33:23 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import LandingPage from "./components/LandingPage.jsx";
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; 
+// HashRouter is safer for GitHub Pages
+
+import LandingPage from "./components/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      {/* stub these until you wire them */}
-      <Route path="/login" element={<div>Login</div>} />
-      <Route path="/register" element={<div>Register</div>} />
-      {/* catch-all */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
   );
 }
